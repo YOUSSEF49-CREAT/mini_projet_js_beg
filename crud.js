47ff6fb6376f81bdf51a66f103e854578bd3b67b
user49-div <mohamedyoussef49574@gmail.com>
2025-11-08 21:45:49 +0100

    feat : finaliser du projet

--- crud.js
+++ crud.js
@@ -7,6 +7,8 @@ let totale = document.getElementById('totale')
 let count = document.getElementById('count')
 let category = document.getElementById('category')
 let submit = document.getElementById('submit')
+let mood = 'create';
+let tmp ;
 
 //  function dial total
 function majmo3(){
@@ -19,7 +21,8 @@ function majmo3(){
     }
 }
 
-// ajou
+//************************************** */ ajou*************************
+
 let arrkhawya ;
 if(localStorage.product != null){
     arrkhawya = JSON.parse(localStorage.product)
@@ -31,49 +34,124 @@ if(localStorage.product != null){
 submit.addEventListener('click', function(){
    
     let objt = {
-        title : title.value,
+        title : title.value.toLowerCase(),
         price : price.value,
         taxes : taxes.value,
         ads : ads.value,
         discount : discount.value,
         totale : totale.innerHTML,
         count : count.value,
-        category : count.value,
+        category : category.value.toLowerCase(),
     }
-    arrkhawya.push(objt)
+    // ***************************validation***************************
+     if(title.value != '' && price.value != '' && category.value != ''){
+             if(mood === 'create'){
+            arrkhawya.push(objt);
+             }
+            else{
+            arrkhawya[ tmp ] = objt ;
+            mood = 'create';
+            submit.innerHTML = 'create'
+            count.style.display = 'block'
+    }
+    title.value = "";
+    price.value = "";
+    taxes.value = "";
+    ads.value = "";
+    discount.value = "";
+    count.value = "";
+    category.value = "";
+    totale.innerHTML = "" ;
+     }
+    // if(arrkhawya.count > 1){
+    //     for(let i=0 ; i < arrkhawya.count ; i++ ){
+    //         arrkhawya.push(objt)
+    //     }
+    // } else{
+    //          arrkhawya.push(objt)
+    //     }
+   
    localStorage.setItem('product', JSON.stringify(arrkhawya))
-   title.value = "";
-   price.value = "";
-   taxes.value = "";
-   ads.value = "";
-   discount.value = "";
-   count.value = "";
-   category.value = "";
-   totale.innerHTML = "" ;
+  
    afich()
    
 })
+// *********************************search****************************************
+let searchMood = 'title' ;
+function recherchmood(id){
+    let search = document.getElementById("search");
+   if(id == 'searchtitle'){
+    searchMood = 'title'
+    search.placeholder ='search by title'
+    search.value = '';
+    afich()
+   }else{
+    searchMood = 'category'
+    search.placeholder ='search by category'
+    search.value = '';
+    afich()
+   }
+    search.focus()
+}
+function recherch(value){ 
+    let table = "" ;
+    if(searchMood == 'title'){
+       for(let i = 0  ; i < arrkhawya.length ; i++){
+         if(arrkhawya[i].title.includes(value.toLowerCase())){
+               table += `
+                            <div id="div_kbira">
+                            <h2>${i+1}</h2>
+                            <h1>${arrkhawya[i].title}</h1>
+                            <div class="div2">
+                                <p>price :${arrkhawya[i].price}</p>
+                                <p>taxes :${arrkhawya[i].taxes}</p>
+                                <p>ads :${arrkhawya[i].ads}</p>
+                                <p>discount :${arrkhawya[i].discount}</p>
+                                <p>total :${arrkhawya[i].totale}</p>
+                                <p>category :${arrkhawya[i].category}</p>
+                            </div>
+                            <div class="div3">
+                                <button onclick="updite(${i})">updit</button>
+                                <button id="del" onclick="deletarr(${i})">delet</button>
+                            </div>
+                            </div>
+                 `
+         }
+       }
+
+    }else{
+           for(let i = 0  ; i < arrkhawya.length ; i++){
+         if(arrkhawya[i].category.includes(value.toLowerCase())){
+               table += `
+                            <div id="div_kbira">
+                            <h2>${i+1}</h2>
+                            <h1>${arrkhawya[i].title}</h1>
+                            <div class="div2">
+                                <p>price :${arrkhawya[i].price}</p>
+                                <p>taxes :${arrkhawya[i].taxes}</p>
+                                <p>ads :${arrkhawya[i].ads}</p>
+                                <p>discount :${arrkhawya[i].discount}</p>
+                                <p>total :${arrkhawya[i].totale}</p>
+                                <p>category :${arrkhawya[i].category}</p>
+                            </div>
+                            <div class="div3">
+                                <button onclick="updite(${i})">updit</button>
+                                <button id="del" onclick="deletarr(${i})">delet</button>
+                            </div>
+                            </div>
+                 `
+         }
+       }
+    }
+    document.getElementById('divmakbar').innerHTML = table ;
+}
 
 
+// *********************************afichage***************************************
  function afich(){
+
+    
     let table = '';
-    // for(let i = 0 ; i < arrkhawya.length ; i++){
-    //     table += `
-    //                         <tr>
-    //                         <td>${i}</td>
-    //                         <td>${arrkhawya[i].title}</td>
-    //                         <td>${arrkhawya[i].price}</td>
-    //                         <td>${arrkhawya[i].taxes}</td>
-    //                         <td>${arrkhawya[i].ads}</td>
-    //                         <td>${arrkhawya[i].discount}</td>
-    //                         <td>${arrkhawya[i].totale}</td>
-    //                         <td>${arrkhawya[i].count}</td>
-    //                         <td>${arrkhawya[i].category}</td>
-    //                         <td><button>updit</button></td>
-    //                         <td><button>delet</button></td>
-    //                         </tr>
-    //     `
-    // }
     for(let i = 0 ; i < arrkhawya.length ; i++){
         table += `
                             <div id="div_kbira">
@@ -85,20 +163,39 @@ submit.addEventListener('click', function(){
                                 <p>ads :${arrkhawya[i].ads}</p>
                                 <p>discount :${arrkhawya[i].discount}</p>
                                 <p>total :${arrkhawya[i].totale}</p>
+                                <p>category :${arrkhawya[i].category}</p>
                             </div>
                             <div class="div3">
-                                <button>updit</button>
+                                <button onclick="updite(${i})">updit</button>
                                 <button id="del" onclick="deletarr(${i})">delet</button>
                             </div>
                             </div>
-        `
+                 `
     }
 
     document.getElementById('divmakbar').innerHTML = table ;
    
    }
+
+// ***********************   ubdite ******************************
        
-  
+  function updite(x){
+       title.value = arrkhawya[x].title ;
+       price.value = arrkhawya[x].price ;
+       taxes.value = arrkhawya[x].taxes ;
+       ads.value = arrkhawya[x].ads ;
+       discount.value = arrkhawya[x].discount ;
+       category.value = arrkhawya[x].category ;
+       count.style.display = 'none'
+       submit.innerHTML = 'updite'
+       majmo3()
+       mood = 'update'
+       tmp = x ;
+       scroll({
+          top:0,
+          behavior:'smooth'
+       })
+  }
 
   function deletarr(i){
     arrkhawya.splice(i,1);
@@ -106,6 +203,7 @@ submit.addEventListener('click', function(){
     afich();
   }
    afich();
+
   
 
 
